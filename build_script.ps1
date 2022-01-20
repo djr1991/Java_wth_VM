7eff036da90c02bf4ce9ecbdac05cf8d3b72e0bf
Luis <6314335+luisfeliz79@users.noreply.github.com>
2022-01-20 12:10:46 -0500

    added custom rdp port, fixed env paths

--- build_script.ps1
+++ build_script.ps1
@@ -22,11 +22,23 @@ $MavenBinPath=(Get-ChildItem -Path "C:\ProgramData\chocolatey\lib\maven" -Filter
 [Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";$MavenBinPath", [EnvironmentVariableTarget]::Machine)
 
 
+# Set AZ CLI enviromental variables
+[Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin", [EnvironmentVariableTarget]::Machine)
+
+# Set JAVA enviromental variables
+$javaPath=(Get-ChildItem -Path "C:\program files\openjdk" -Directory).fullname
+[Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";$javaPath\bin", [EnvironmentVariableTarget]::Machine)
+
+# Set Git enviromental variables
+[Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";C:\Program Files\Git\bin", [EnvironmentVariableTarget]::Machine)
+
+
+
 #PreAdd FW rules java run times
 $ListOfPaths=@()
 
 $ListOfPaths+=Get-ChildItem -Path "C:\program files\openjdk" -Filter java.exe  -Recurse
-$ListOfPaths+=Get-ChildItem -Path "$home\.vscode" -Filter java.exe  -Recurse
+$ListOfPaths+=Get-ChildItem -Path "c:\users\adminUsername\.vscode" -Filter java.exe  -Recurse
 
 $ListOfPaths | foreach {
 
@@ -59,17 +71,27 @@ $ListOfPaths | foreach {
 
 }
 
+#Set a Custom RDP Port
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "fDenyTSConnections" -Value  0
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value 22389
+New-NetFirewallRule -DisplayName 'RDPPORTLatest' -Profile 'Any' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22389
+Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\WindowsFirewall\PublicProfile' -name "AllowLocalPolicyMerge" -Value 1
+
+reg query "HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile" /s
+Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber"
+Get-NetFirewallProfile -PolicyStore ActiveStore
+
 
 #Copy Hackathon Files ...
 Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/djr1991/Java_wth_VM/raw/main/spring-petclinic.zip" -OutFile petclinic.zip
-mkdir $home\desktop\petclinic -force
-Expand-Archive -Path petclinic.zip -DestinationPath $home\desktop\petclinic
+mkdir c:\users\adminusername\desktop\petclinic -force
+Expand-Archive -Path petclinic.zip -DestinationPath c:\users\adminusername\desktop\petclinic
 
 
 
 #Create a shortcut to the command prompt
 $SourceFilePath = "C:\windows\system32\cmd.exe"
-$ShortcutPath = "$home\desktop\Command Prompt.lnk"
+$ShortcutPath = "c:\users\adminusername\desktop\Command Prompt.lnk"
 $WScriptObj = New-Object -ComObject ("WScript.Shell")
 $shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
 $shortcut.TargetPath = $SourceFilePath
